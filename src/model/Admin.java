e8d08c4ce977020dd5e6fd9b51abe34bee247157
Harsha <nanduarvi111@gmail.com>
2026-06-21 21:06:00 +0530

    Added Admin model

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -1,5 +1,20 @@
 package model;
 
-public class Admin {
+public class Admin extends User {
+    private String department;
+
+    public Admin(int id,String name,String department){
+        super(id,name);
+        this.department=department;
+
+    }
+
+    public void displayAdmin(){
+        System.out.println("\n");
+        System.out.println("-----Admin profile-----");
+        super.displayuser();
+        System.out.println("department"+department);
+        System.out.println("-----------------------");
+    }
     
 }
