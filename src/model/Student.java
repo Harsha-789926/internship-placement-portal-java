a640de6de02648e4d2716334f0ec563626572e71
Harsha <nanduarvi111@gmail.com>
2026-06-21 21:05:12 +0530

    Added Student model

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,23 @@
 package model;
 
-public class Student {
-    
+public class Student extends User {
+    private String branch;
+    private double cgpa;
+    private String skills;
+    public Student(int id,String name,String branch,double cgpa,String skills){
+        super(id,name);
+        this.branch=branch;
+        this.cgpa=cgpa;     
+        this.skills=skills;
+    }
+
+    public void displaystudent(){
+        System.out.println("\n");
+        System.out.println("----Student Details-----");
+        super.displayuser();
+        System.out.println("Branch:"+branch);
+        System.out.println("CGPA:"+cgpa);
+        System.out.println("Skills:"+skills);
+        System.out.println("-----------------------");
+    }
 }
