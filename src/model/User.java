ebb511c0df47b88afc307d65684619d0eaff350d
Harsha <nanduarvi111@gmail.com>
2026-06-21 21:04:37 +0530

    Added user model

--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,26 @@
 package model;
 
 public class User {
-    
+    protected int id;
+    protected String name;
+
+    public User(int id,String name){
+        this.id=id;
+        this.name=name;
+    }
+
+    public void  displayuser(){
+        System.out.println("User ID: " + id);
+        System.out.println("User Name: " + name);
+        
+    }
+
+    public void displayonlyuser(){
+        System.out.println("\n");
+        System.out.println("-----User Details-----");
+        System.out.println("User ID: " + id);
+        System.out.println("User Name: " + name);
+        System.out.println("-----------------------");
+    }
 }
+
