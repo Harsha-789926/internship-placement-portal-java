f21f40d0d8e2cbccc7142018364b67408fe623f6
Harsha <nanduarvi111@gmail.com>
2026-06-21 21:07:19 +0530

    Added Application model

--- src/model/Application.java
+++ src/model/Application.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Application {
-    
+    private int studentId;
+    private int jobId;
+    private String status; // e.g., "Pending", "Accepted", "Rejected"
+
+    public Application(int studentId,int jobId,String status){
+        this.studentId=studentId;
+        this.jobId=jobId;
+        this.status=status;
+    }
+
+    public void displayApplication(){
+        System.out.println("\n");
+        System.out.println("-----Application Details-----");
+        System.out.println("Student ID:"+studentId);
+        System.out.println("Job ID:"+jobId);
+        System.out.println("Status:"+status);
+        System.out.println("-----------------------------");
+    }
 }
