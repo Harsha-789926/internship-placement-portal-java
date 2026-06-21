f629bb9f6ec7da32dbd0a45a19af1c9e29904b02
Harsha <nanduarvi111@gmail.com>
2026-06-21 21:06:50 +0530

    Added Job model

--- src/model/Job.java
+++ src/model/Job.java
@@ -1,5 +1,24 @@
 package model;
 
 public class Job {
-    
+    private int id;
+    private String title;
+    private String company;
+    private String skillsrequired;
+
+    public Job(int id,String title,String company,String skillsrequired){
+        this.id=id;
+        this.title=title;
+        this.company=company;
+        this.skillsrequired=skillsrequired;
+    }
+    public void displayJob(){
+        System.out.println("\n");
+        System.out.println("-----Job Details-----");
+        System.out.println("Job Id:"+id);
+        System.out.println("Title:"+title);
+        System.out.println("Company:"+company);
+        System.out.println("Skills Required:"+skillsrequired);
+        System.out.println("-----------------------------");
+    }
 }
